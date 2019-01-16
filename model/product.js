0533d220181467a851bbed6ff077a15be77aa036
mrphemi <mrlapinho@gmail.com>
2019-01-16 02:56:32 +0100

    Add product model

---
+++ model/product.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+   name: {
+      type: String
+   },
+   desc: {
+      type: String
+   },
+   price: {
+      type: Number
+   },
+   image: {
+      type: String
+   },
+   createdAt: {
+      type: Date,
+      default: Date.now
+   }
+});
+
+const Product = mongoose.model("product", productSchema);
+
+module.exports = Product;
