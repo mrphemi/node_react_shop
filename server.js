3a1e8a6b3ba39eb941ec8991674b7721011d38b4
mrphemi <mrlapinho@gmail.com>
2019-01-16 02:28:53 +0100

    Add cors dependency, enable cors and connect to mongodb

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const PORT = 5000;
+
+mongoose.connect(
+   "mongodb://localhost:27017/shop",
+   { useNewUrlParser: true }
+);
+mongoose.Promise = global.Promise;
 
 // Initialize express
 const app = express();
 
 // json parser
 const jsonParser = bodyParser.json();
+
+// Enable cors
+app.use(cors);
 
 // Parse Json requests
 app.use(jsonParser);
