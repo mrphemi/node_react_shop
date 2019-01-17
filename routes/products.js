a483611b43f1cdd3651cc3ef468dfc62f5e10552
mrphemi <mrlapinho@gmail.com>
2019-01-17 03:21:58 +0100

    Add route for updating product records

--- routes/products.js
+++ routes/products.js
@@ -114,4 +114,31 @@ router.delete("/:productId", (req, res) => {
       });
 });
 
+// @route   PATCH /products
+// @desc    Updates specified product
+// @access  Private
+router.patch("/:productId", (req, res) => {
+   const productId = req.params.productId;
+   Product.findByIdAndUpdate(productId, req.body, { new: true })
+      .exec()
+      .then(product => {
+         if (product) {
+            res.status(200).json({
+               message: "Product updated sucessfully",
+               product
+            });
+         } else {
+            res.status(404).json({
+               message: "No matches for product"
+            });
+         }
+      })
+      .catch(err => {
+         res.status(500).json({
+            message: "An error occured",
+            err
+         });
+      });
+});
+
 module.exports = router;
