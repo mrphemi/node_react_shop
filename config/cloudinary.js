609a990bea367381a45169890809dfc3c70c1bf1
mrphemi <mrlapinho@gmail.com>
2019-01-26 20:52:31 +0100

    Create cloudinary config file

---
+++ config/cloudinary.js
@@ -0,0 +1,12 @@
+import cloudinary from "cloudinary";
+
+const cloudinaryConfig = (req, res, next) => {
+   cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET
+   });
+   next();
+};
+
+export default cloudinary;
