5d386588b0c39a3fa21e15c4047396bb3d36243c
mrphemi <mrlapinho@gmail.com>
2019-02-01 03:34:24 +0100

    Initialize client with CRA and install dependencies

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+   render() {
+      return (
+         <div className="App">
+            <h1>Node React shop</h1>
+         </div>
+      );
+   }
+}
+
+export default App;
