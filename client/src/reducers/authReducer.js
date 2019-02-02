371ab1cf1d1bfdaf6ae382fa1d2163e1ec1f361d
mrphemi <mrlapinho@gmail.com>
2019-02-02 21:44:05 +0100

    Add auth reducer for login

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,24 @@
-const INITIAL_STATE = [];
+import { LOGIN_ERROR, SET_CURRENT_USER } from "../actions/types";
+
+const INITIAL_STATE = {
+   isAuthenticated: false,
+   user: {}
+};
 
 function authReducer(state = INITIAL_STATE, action) {
    switch (action.type) {
+      case SET_CURRENT_USER:
+         return {
+            ...state,
+            isAuthenticated: Boolean(Object.keys(action.user).length),
+            user: action.user
+         };
+      case LOGIN_ERROR:
+         return {
+            ...state,
+            isAuthenticated: false,
+            user: {}
+         };
       default:
          return state;
    }
