cbfde21ce2bc8be27e777b0229848d4bfe37fda4
mrphemi <mrlapinho@gmail.com>
2019-02-09 21:13:58 +0100

    Add extra action type constants

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,5 @@
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const LOGIN_ERROR = "LOGIN_ERROR";
+export const SIGNUP_ERROR = "SIGNUP_ERROR";
+export const SHOW_MSG = "SHOW_MSG";
+export const SHOW_ERR_MSG = "SHOW_ERR_MSG";
