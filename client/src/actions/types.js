9afa0bdff6bcd1ec8f77df372047e50d93aeb1e7
mrphemi <mrlapinho@gmail.com>
2019-02-10 21:17:00 +0100

    Add product actions and types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -3,3 +3,5 @@ export const LOGIN_ERROR = "LOGIN_ERROR";
 export const SIGNUP_ERROR = "SIGNUP_ERROR";
 export const SHOW_MSG = "SHOW_MSG";
 export const SHOW_ERR_MSG = "SHOW_ERR_MSG";
+export const GET_PRODUCTS = "GET_PRODUCTS";
+export const GET_PRODUCT = "GET_PRODUCT";
